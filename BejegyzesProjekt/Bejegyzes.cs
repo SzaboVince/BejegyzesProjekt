d45c8a2cd4d29601a145cb32c12484f74912604b
Szabó Vince <vince20020622@gmail.com>
2023-09-26 08:42:40 +0200

    1. feladat megoldása

---
+++ BejegyzesProjekt/Bejegyzes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProjekt
+{
+    internal class Bejegyzes
+    {
+        private string szerzo;
+        private string tartalom;
+        private int likeok;
+        private DateTime letrejott;
+        private DateTime szerkesztve;
+
+        public Bejegyzes(string szerzo, string tartalom, int likeok, DateTime letrejott, DateTime szerkesztve)
+        {
+            this.szerzo = szerzo;
+            this.tartalom = tartalom;
+            this.likeok = 0;
+            this.letrejott = DateTime.Now;
+            this.szerkesztve = DateTime.Now;
+        }
+
+        public string Szerzo { get => szerzo;}
+        public string Tartalom { get => tartalom; set => tartalom = value; }
+        public int Likeok { get => likeok; }
+        public DateTime Letrejott { get => letrejott;}
+        public DateTime Szerkesztve { get => szerkesztve;}
+
+        public void Like()
+        {
+            this.likeok++;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.szerzo}-{this.likeok}-{this.letrejott}\nSzerkesztve: {this.szerkesztve}\n{this.tartalom}";
+        }
+    }
+}
