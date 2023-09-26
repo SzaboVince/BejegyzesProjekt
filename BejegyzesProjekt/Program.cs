e5b6a9c7f8dc6d6152bb94578e4c67fa8b932bbb
Szabó Vince <vince20020622@gmail.com>
2023-09-26 09:57:14 +0200

    Be van fejezve a nagy mű, igen. A gép forog, az alkotó pihen.

--- BejegyzesProjekt/Program.cs
+++ BejegyzesProjekt/Program.cs
@@ -9,9 +9,9 @@ namespace BejegyzesProjekt
 {
     internal class Program
     {
-        static void f2()
+        static List<Bejegyzes> bejegyzesek = new List<Bejegyzes>();
+        static void hozzaad()
         {
-            List<Bejegyzes> bejegyzesek = new List<Bejegyzes>();
             bejegyzesek.Add(new Bejegyzes("Immanuel Kant", "Szép az, ami érdek nélkül tetszik."));
             bejegyzesek.Add(new Bejegyzes("Georg Wilhelm Friedrich Hegel", "Szavakban gondolkodunk, mert a gondolat a szónak köszönheti legmagasabb rendű és legigazibb létét."));
             
@@ -25,16 +25,21 @@ namespace BejegyzesProjekt
                     string tartalom = Console.ReadLine();
                     bejegyzesek.Add(new Bejegyzes(szerzo, tartalom));
                 }
-            
-            string[] sorok = File.ReadAllLines("bejegyzesek.csv");
-            foreach (string i in sorok)
+             }
+        static void beolvas()
+        {
+            StreamReader sr = new StreamReader("bejegyzesek.csv");
+            while (!sr.EndOfStream)
             {
-                string[] reszek = i.Split(';');
-                    string szerzo = reszek[0];
-                    string tartalom = reszek[1];
-                    bejegyzesek.Add(new Bejegyzes(szerzo, tartalom));
+                string[] adatok = sr.ReadLine().Split(';');
+                string a = adatok[0];
+                string b = adatok[1];
+                Bejegyzes bej=new Bejegyzes(a,b);
+                bejegyzesek.Add(bej);
             }
-            
+        }
+        static void like()
+        {
             Random rnd = new Random();
             int likeki = bejegyzesek.Count * 20;
             for (int i = 0; i < likeki; i++)
@@ -42,20 +47,85 @@ namespace BejegyzesProjekt
                 int index = rnd.Next(bejegyzesek.Count);
                 bejegyzesek[index].Like();
             }
-
+        }    
+        static void masodikbejegyzesmodositasa()
+        {
             Console.Write("Adja meg a második bejegyzés új tartalmát: ");
             string uj = Console.ReadLine();
             bejegyzesek[1].Tartalom = uj;
+        }
 
+        static void kiir()
+        {
             foreach (Bejegyzes item in bejegyzesek)
             {
                 Console.WriteLine(item);
             }
         }
+        
+
+        static void f3()
+        {
+            int legnepszerubb = 0;
+            foreach (var item in bejegyzesek)
+            {
+                if (item.Likeok>legnepszerubb)
+                {
+                    legnepszerubb = item.Likeok;
+                }
+            }
+            Console.WriteLine($"Legnépszerübb bejegyzés likeainak száma: {legnepszerubb}.");
+
+
+            bool a = false;
+            foreach (var item in bejegyzesek)
+            {
+                if (item.Likeok>35)
+                {
+                    a = true;
+                }
+            }
+            if (a==true)
+            {
+                Console.WriteLine("Van olyan bejegyzés, ami 35-nél több likeot kapott.");
+            }
+            else
+            {
+                Console.WriteLine("Nincs olyan bejegyzés, ami 35-nél több likeot kapott.");
+            }
+
+            int b = 0;
+            foreach (var item in bejegyzesek)
+            {
+                if (item.Likeok < 15)
+                {
+                    b++;
+                }
+            }
+            Console.WriteLine($"{b} olyan bejegyzés van, ami 15-nél kevesebb likeot kapott");
+
+            Console.WriteLine("------------------------------------------------------------------------------");
+
+            var rendezett = bejegyzesek.OrderByDescending(c => c.Likeok).ToList();
+            foreach (var item in rendezett)
+            {
+                Console.WriteLine(item);
+            }
+            StreamWriter sw = new StreamWriter("bejegyzesek_rendezett.txt");
+            foreach (var item in rendezett)
+            {
+                sw.WriteLine(item);
+            }
+        }
 
         static void Main(string[] args)
         {
-            f2();
+            hozzaad();
+            beolvas();
+            like();
+            masodikbejegyzesmodositasa();
+            kiir();
+            f3();
             Console.ReadLine();
 
         }
